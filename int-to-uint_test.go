24bbade1d4b645d93f684fc34c92116b3e332bd1
Yuda Cogati <yudapc@gmail.com>
2019-07-23 11:25:59 +0700

    added test int to uint

---
+++ int-to-uint_test.go
@@ -0,0 +1,16 @@
+package tdd
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestIntToUint(test *testing.T) {
+	var value int = 42
+	valueType := IntToUint(value)
+	typeValue := reflect.TypeOf(valueType).Kind()
+	if typeValue != reflect.Uint {
+		test.Errorf("Value type is not uint")
+	}
+
+}
