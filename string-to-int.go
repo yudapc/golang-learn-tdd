664a59b06288008aeabc0e8a3e590534240bf38b
Yuda Cogati <yuda.pc@gmail.com>
2021-03-12 00:31:43 +0700

    Update function StringToInt with default value 0 when string is not number

--- string-to-int.go
+++ string-to-int.go
@@ -4,7 +4,11 @@ import (
 	"strconv"
 )
 
+// StringToInt is function for convert everything string to int
 func StringToInt(value string) int {
-	valueToInt, _ := strconv.Atoi(value)
+	valueToInt, err := strconv.Atoi(value)
+	if err != nil {
+		return 0
+	}
 	return valueToInt
 }
--- string-to-int_test.go
+++ string-to-int_test.go
@@ -1,15 +1,29 @@
 package tdd
 
 import (
-	"reflect"
 	"testing"
 )
 
-func TestStringToInt(test *testing.T) {
-	value := "12"
-	valueType := StringToInt(value)
-	typeValue := reflect.TypeOf(valueType).Kind()
-	if typeValue != reflect.Int {
-		test.Errorf("Value type is not int")
-	}
+func TestStringToInt(t *testing.T) {
+	t.Run("should return int when string is number", func(t *testing.T) {
+		input := "12"
+		value := StringToInt(input)
+		if value != 12 {
+			t.Errorf("Value type is not int")
+		}
+	})
+	t.Run("should return 0 when invalid number", func(t *testing.T) {
+		input := "ngaco"
+		value := StringToInt(input)
+		if value != 0 {
+			t.Errorf("Value type is not int")
+		}
+	})
+	t.Run("should return 0 when combination string and number", func(t *testing.T) {
+		input := "ngaco1"
+		value := StringToInt(input)
+		if value != 0 {
+			t.Errorf("Value type is not int")
+		}
+	})
 }
